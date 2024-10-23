c6505ebcea70dd107bdfc7d3ecb47b5738332827
Utkarsh  Mehta <utkarshmehta4991@gmail.com>
2024-10-23 23:40:10 +0530

    ui-textview modifiers

--- swift-ui-development/textview.swift
+++ swift-ui-development/textview.swift
@@ -9,10 +9,19 @@ import SwiftUI
 
 struct textview: View {
     var body: some View {
-        Text("Utkarsh Mehta")
-            .font(.title)
-            .foregroundColor(.red)
-            .fontWeight(.bold)
+        Text("Utkarsh Mehta is currently learning IOS for dev in deloitte")
+//            .font(.title)
+//            .foregroundColor(.black)
+//            .fontWeight(.bold)
+//            .underline(true , color: .green)
+//            .italic()
+//            .strikethrough(true,color: .red)
+            .font(.system(size: 24,weight: .semibold,design: .serif))
+            .baselineOffset(10)// space bw two rows
+            .kerning(10) //space between two words
+            .multilineTextAlignment(.center)
+            
+        
     }
 }
 
